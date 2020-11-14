cb806d3d34164c892c6f47b11fe1d463888fece8
HMC <1170313@naver.com>
2020-11-14 22:30:47 +0900

    setting, 물탱크수위 Activity생성

---
+++ PlantEngine_TermProject/app/src/main/java/com/example/plantengine_termproject/WaterTankActivity.kt
@@ -0,0 +1,11 @@
+package com.example.plantengine_termproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterTankActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_tank)
+    }
+}
